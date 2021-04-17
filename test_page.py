f305980e40f90d04a9da42a3e1008c9bd044732f
Igor <59920282+hello-igor@users.noreply.github.com>
2021-04-17 17:34:15 +0700

    Added page, edited page_size

---
+++ test_page.py
@@ -0,0 +1,29 @@
+import pytest
+import allure
+from src.load_data import load
+
+
+
+class TestPage:
+
+    @allure.feature("Тестирование параметра page")
+    @allure.story("Позитивные кейсы")
+    @pytest.mark.parametrize("data", [x for x in load("test_page_positive.json")])
+    def test_positive(self, regions_api, data):
+        allure.dynamic.title(data["title"])
+        response = regions_api.get(params={"page":data["value"]})
+        response = response.json()
+        for regions in response["items"]:
+            with allure.step(f'Проверка названий элементов на странице: {regions["name"]}'):
+                assert regions["name"] in data["expected_value"]
+'''
+    @allure.feature("Тестирование параметра page")
+    @allure.story("Негативный кейсы")
+    @pytest.mark.parametrize("data", [x for x in load("test_page_negative.json")])
+    def test_negative(self, regions_api, data):
+        allure.dynamic.title(data["title"])
+        response = regions_api.get(params={"page":data["value"]})
+        response = response.json()
+        with allure.step(f'Проверка возвращаемой ошибки: {response["error"]["message"]}'):
+            assert response["error"]["message"] in data["expected_value"]
+'''
--- test_page_size.py
+++ test_page_size.py
@@ -11,7 +11,10 @@ class TestPageSize:
     @pytest.mark.parametrize("data", [x for x in load("test_page_size_positive.json")])
     def test_positive(self, regions_api, data):
         allure.dynamic.title(data["title"])
-        response = regions_api.get(params={"page_size":data["value"]})
+        if data["value"] == "Default":
+            response = regions_api.get()
+        else:
+            response = regions_api.get(params={"page_size":data["value"]})
         response = response.json()
         with allure.step(f'Проверка количества элементов на странице: {len(response["items"])}'):
             assert len(response["items"]) == data["expected_value"]
